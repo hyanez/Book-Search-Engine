d8a405a59736bad858dda057d7d7f907ccb8d3a5
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-02 13:01:50 -0500

    Exported resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,7 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
+
+const resolvers = {};
+
+module.exports = resolvers;
