7ee01a47ed9e712205b0ab1759aa0f99315647ce
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-02 00:34:58 -0500

    Created User type, Book type, and Auth type in typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  # Defining a User.
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID!
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
