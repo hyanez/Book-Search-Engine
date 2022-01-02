c41192c352d580a3c86474fdf62845095e0ab5d4
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-02 14:16:27 -0500

    created ADD_USER mutation in mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,6 +13,27 @@ export const LOGIN_USER = gql`
 `;
 
 // ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
 
 // SAVE_BOOK will execute the saveBook mutation.
 
