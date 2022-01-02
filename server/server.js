81cb8cebcd8d8f6031fe439319f3347f6ffe74bb
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-01 23:26:29 -0500

    created a new apollo server with typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
