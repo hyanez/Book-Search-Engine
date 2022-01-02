41ee99548f10337839471514ab5d3f0ac3e0e373
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-02 13:37:40 -0500

    created LOGIN_USER mutation in mutations.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 
+//This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
 export const GET_ME = gql`
   query me {
     _id
