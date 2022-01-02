af85ef87734cf474841972ac69aed679146de73f
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-02 13:13:03 -0500

    Created Query for single user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,16 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const data = User.findOne({ _id: context.user._id });
+        return data;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
 
 module.exports = resolvers;
