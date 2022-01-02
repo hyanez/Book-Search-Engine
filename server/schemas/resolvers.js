a4ab2b15c6ed7d4adf38fb70c8bfcc0fc78a4107
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-02 13:17:57 -0500

    Created login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,6 +12,25 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
   },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No User with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
